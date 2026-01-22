ae85e71f611dbf87c485174f8e674b7f7fff7aba
Abi <abiofficial1805@gmail.com>
2026-01-22 12:03:22 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Web + API + AI</h1>
+      <p>Frontend is running ✅</p>
+    </div>
+  );
+}
+
+export default App;
